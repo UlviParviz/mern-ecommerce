1c99f97f3d376aeee3bbb0d28a4e9abc221e63c7
Ulvi Spaethe <ulvispaethe@gmail.com>
2024-05-01 22:17:36 +0100

    added user controllers and routes

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -4,54 +4,53 @@ import { getResetPasswordTemplate } from "../utils/emailTemplate.js";
 import ErrorHandler from "../utils/errorHandler.js";
 import sendEmail from "../utils/sendEmail.js";
 import sendToken from "../utils/sendToken.js";
-import crypto from 'crypto'
+import crypto from "crypto";
 
 //Register
 export const registerUser = catchAsyncErrors(async (req, res, next) => {
   const { name, email, password } = req.body;
   const user = await User.create({ name, email, password });
 
-  sendToken(user,201,res)
-
+  sendToken(user, 201, res);
 });
 
 //Login
 export const loginUser = catchAsyncErrors(async (req, res, next) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    if(!email || !password){
-        return next(ErrorHandler('Please enter a valid email address and password'), 400);
-    }
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter email & password", 400));
+  }
 
-    const user = await User.findOne({email}).select('+password')
+  // Find user in the database
+  const user = await User.findOne({ email }).select("+password");
 
-    if(!user){
-        return next(ErrorHandler('Invalid email or password'), 401)
-    }
+  if (!user) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
 
-    //Check password
-    const isPasswordMatched = await user.comparePassword(password)
+  // Check if password is correct
+  const isPasswordMatched = await user.comparePassword(password);
 
-    if(!isPasswordMatched){
-        return next(ErrorHandler('Invalid email or password'), 401)
-    }
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid email or password", 401));
+  }
 
-  sendToken(user,200,res)
+  sendToken(user, 200, res);
+});
 
+export const logout = catchAsyncErrors(async (req, res, next) => {
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
   });
 
-  export const logout = catchAsyncErrors(async (req, res, next) =>{
-    res.cookie("token", null, {
-      expires: new Date(Date.now()),
-      httpOnly: true
-    })
+  res.status(200).json({
+    message: "Logged Out",
+  });
+});
 
-    res.status(200).json({
-      message: 'Logged Out'
-    })
-  })
-
-// Forgot password   
+// Forgot password
 export const forgotPassword = catchAsyncErrors(async (req, res, next) => {
   // Find user in the database
   const user = await User.findOne({ email: req.body.email });
@@ -125,4 +124,95 @@ export const resetPassword = catchAsyncErrors(async (req, res, next) => {
   await user.save();
 
   sendToken(user, 200, res);
+});
+
+export const getUserProfile = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req?.user?._id);
+
+  res.status(200).json({
+    user,
+  });
+});
+
+export const updatePassword = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req?.user?._id).select("+password");
+
+  const isPasswordMatched = await user.comparePassword(req.body.oldPassword);
+
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Old password mismatch", 400));
+  }
+
+  user.password = req.body.password;
+
+  user.save();
+
+  res.status(200).json({
+    success: true,
+  });
+});
+
+export const updateProfile = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user._id, newUserData, {
+    new: true,
+  });
+
+  res.status(200).json({
+    user,
+  });
+});
+
+export const allUsers = catchAsyncErrors(async (req, res, next) => {
+  const users = await User.find();
+
+  res.status(200).json({
+    users,
+  });
+});
+
+export const getUserDetail = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if(!user){
+    return next(new ErrorHandler(`User not found with id: ${req.params.id}`, 404));
+  }
+
+  res.status(200).json({
+    user,
+  });
+});
+
+export const updateUser = catchAsyncErrors(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+    role: req.body.role,
+  };
+
+  const user = await User.findByIdAndUpdate(req.params.id, newUserData, {
+    new: true,
+  });
+
+  res.status(200).json({
+    user,
+  });
+});
+
+export const deleteUser = catchAsyncErrors(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if(!user){
+    return next(new ErrorHandler(`User not found with id: ${req.params.id}`, 404));
+  }
+
+  await user.deleteOne()
+
+  res.status(200).json({
+    success: true
+  });
 });
--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,16 +1,38 @@
 import express from "express";
-import { forgotPassword, loginUser, logout, registerUser, resetPassword } from "../controllers/auth.controllers.js";
+import {
+  allUsers,
+  deleteUser,
+  forgotPassword,
+  getUserDetail,
+  getUserProfile,
+  loginUser,
+  logout,
+  registerUser,
+  resetPassword,
+  updatePassword,
+  updateProfile,
+  updateUser,
+} from "../controllers/auth.controllers.js";
+import { authorizeRoles, isAuthendicatedUser } from "../middlewares/auth.js";
 const router = express.Router();
 
-router.route("/register").post(registerUser)
-router.route("/login").post(loginUser)
-router.route("/logout").get(logout)
+router.route("/register").post(registerUser);
+router.route("/login").post(loginUser);
+router.route("/logout").get(logout);
 
-router.route("/password/forgot").post(forgotPassword)
-router.route("/password/reset/:token").put(resetPassword)
+router.route("/password/forgot").post(forgotPassword);
+router.route("/password/reset/:token").put(resetPassword);
+router.route("/me").get(isAuthendicatedUser, getUserProfile);
+router.route("/me/update").put(isAuthendicatedUser, updateProfile);
+router.route("/password/update").put(isAuthendicatedUser, updatePassword);
 
+router
+  .route("/admin/users")
+  .get(isAuthendicatedUser, authorizeRoles("admin"), allUsers);
+router
+  .route("/admin/users/:id")
+  .get(isAuthendicatedUser, authorizeRoles("admin"), getUserDetail)
+  .put(isAuthendicatedUser, authorizeRoles("admin"), updateUser)
+  .delete(isAuthendicatedUser, authorizeRoles("admin"), deleteUser);
 
-
-
-
-export default router
+export default router;
