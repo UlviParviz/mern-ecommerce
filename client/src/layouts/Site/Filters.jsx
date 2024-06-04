5229900bef27882e868213aaba821b81910b09e4
Ulvi Spaethe <ulvispaethe@gmail.com>
2024-06-04 13:58:54 +0100

    build again

--- client/src/layouts/Site/Filters.jsx
+++ client/src/layouts/Site/Filters.jsx
@@ -9,12 +9,11 @@ const Filters = () => {
   const [max, setMax] = useState("");
 
   const navigate = useNavigate();
-
   let [searchParams] = useSearchParams();
 
   useEffect(() => {
-    searchParams.has("min") && setMin(searchParams.get("min"));
-    searchParams.has("max") && setMax(searchParams.get("max"));
+    if (searchParams.has("min")) setMin(searchParams.get("min"));
+    if (searchParams.has("max")) setMax(searchParams.get("max"));
   }, [searchParams]);
 
   const handleClick = (checkbox) => {
@@ -58,6 +57,7 @@ const Filters = () => {
 
     return false;
   };
+
   return (
     <div className="flex flex-col gap-3">
       <h2 className="text-3xl font-bold text-center lg:text-left">Filters</h2>
@@ -66,7 +66,7 @@ const Filters = () => {
         <h3 className="text-2xl font-bold">Price</h3>
         <form className="flex gap-3 items-center justify-center" onSubmit={handleButtonClick}>
           <input
-            className="w-[85px]  rounded-lg"
+            className="w-[85px] rounded-lg"
             type="text"
             placeholder="Min ($)"
             value={min}
@@ -86,46 +86,44 @@ const Filters = () => {
         <div className="flex flex-col gap-3">
           <h3 className="text-2xl font-bold">Category</h3>
           <div className="flex flex-wrap gap-3 justify-center lg:block">
-          {PRODUCT_CATEGORIES?.map((category, index) => (
-            <div key={index} className="flex gap-3 items-center">
-              <input
-                defaultChecked={defaultChecHandler("category", category)}
-                name="category"
-                value={category}
-                type="checkbox"
-                onClick={(e) => handleClick(e.target)}
-              />
-              <label htmlFor="">{category}</label>
-            </div>
-          ))}
+            {PRODUCT_CATEGORIES?.map((category, index) => (
+              <div key={index} className="flex gap-3 items-center">
+                <input
+                  defaultChecked={defaultChecHandler("category", category)}
+                  name="category"
+                  value={category}
+                  type="checkbox"
+                  onClick={(e) => handleClick(e.target)}
+                />
+                <label htmlFor="">{category}</label>
+              </div>
+            ))}
           </div>
         </div>
         <div className="flex flex-col gap-3">
           <h3 className="text-2xl font-bold">Ratings</h3>
           <div className="flex flex-wrap gap-3 justify-center lg:block">
-
-          {[5, 4, 3, 2, 1].map((rating, index) => {
-            return( <div key={index} className="flex gap-3 items-center">
-              <input
-                type="checkbox"
-                name="ratings"
-                value={rating}
-                onClick={(e) => handleClick(e.target)}
-                defaultChecked={defaultChecHandler("ratings", rating?.toString())}
-
-              />
-              <label htmlFor="">
-                <span>
-                <Rating
-              name="half-rating-read"
-              value={rating}
-              precision={0.1}
-              readOnly
-            />
-                </span>
-              </label>
-            </div>)
-          })}
+            {[5, 4, 3, 2, 1].map((rating, index) => (
+              <div key={index} className="flex gap-3 items-center">
+                <input
+                  type="checkbox"
+                  name="ratings"
+                  value={rating}
+                  onClick={(e) => handleClick(e.target)}
+                  defaultChecked={defaultChecHandler("ratings", rating?.toString())}
+                />
+                <label htmlFor="">
+                  <span>
+                    <Rating
+                      name="half-rating-read"
+                      value={rating}
+                      precision={0.1}
+                      readOnly
+                    />
+                  </span>
+                </label>
+              </div>
+            ))}
           </div>
         </div>
       </div>
